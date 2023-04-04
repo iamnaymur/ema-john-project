a6a0f279ed268375fd446911f58679321d4849c9
iamnaymur <nrtusher1@gmail.com>
2023-04-04 22:47:37 +0600

    delete product

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Layout/Home';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
+import cartProductsLoader from './loaders/cartProductsLoader';
 
 
 const router = createBrowserRouter([
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
         element:<Shop></Shop>,
       }, {
         path:'orders',
-        element:<Orders></Orders>
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
       },
       {
         path:'inventory',
